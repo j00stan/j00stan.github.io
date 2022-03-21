025212b4cd3d3bfb8b59e74cfc65fef676d9b3d9
j00stan <98915813+j00stan@users.noreply.github.com>
2022-03-21 18:44:21 -0400

    updated

--- js/observer.js
+++ js/observer.js
@@ -6,21 +6,20 @@ const appearOptions = {
     threshold: .25
 };
 
-const appearOnScroll = new IntersectionObserver
-(function(
-    entries, 
+const appearOnScroll = new IntersectionObserver(function(
+    entries,
     appearOnScroll
-) {
+  ) {
     entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target);
-        }
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
     });
-},
-appearOptions);
+  },
+  appearOptions);
 
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
