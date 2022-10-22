4d59624702ec76872384aef55c050c9311eb8d39
j00stan <98915813+j00stan@users.noreply.github.com>
2022-10-22 17:47:14 -0400

    fixed

--- js/medium-zoom.js
+++ js/medium-zoom.js
@@ -1,7 +1,7 @@
 const images = Array.from(document.querySelectorAll("img"));
 images.forEach(img => {
     mediumZoom(img, {
-      margin: 0, /* The space outside the zoomed image */
+      margin: 64, /* The space outside the zoomed image */
       background: "#fff", /* The background of the overlay */
       scrollOffset: 40, /* The number of pixels to scroll to close the zoom */
       container: null, /* Advanced - The viewport to render the zoom in */
