68f5f5803c8cd0b4624b6deaec8fc0d902d4cb39
j00stan <98915813+j00stan@users.noreply.github.com>
2022-10-22 17:43:34 -0400

    img

--- js/medium-zoom.js
+++ js/medium-zoom.js
@@ -1,4 +1,4 @@
-const images = Array.from(document.querySelectorAll("figure img"));
+const images = Array.from(document.querySelectorAll("img"));
 images.forEach(img => {
     mediumZoom(img, {
       margin: 0, /* The space outside the zoomed image */
