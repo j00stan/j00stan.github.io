ca6e4ab09024a4f34a6327c2c61db3e8f85771a7
j00stan <98915813+j00stan@users.noreply.github.com>
2022-10-22 17:37:26 -0400

    added medium zoom

---
+++ js/medium-zoom.js
@@ -0,0 +1,9 @@
+images.forEach(img => {
+    mediumZoom(img, {
+      margin: 0, /* The space outside the zoomed image */
+      background: "#fff", /* The background of the overlay */
+      scrollOffset: 40, /* The number of pixels to scroll to close the zoom */
+      container: null, /* Advanced - The viewport to render the zoom in */
+      template: null /* Advanced - The template element to display on zoom */
+    });
+  });
